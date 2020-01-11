793f72de6a11203930e7da0e3d49236045c585cd
mscbr <scibor.maciej@gmail.com>
2020-01-11 15:18:43 +0100

    frontend init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Users from './user/pages/Users';
 import './App.css';
 
 const App: React.FC = () => {
-    return <div className="App"></div>;
+    return (
+        <Router>
+            <Route path="/">
+                <Users />
+            </Route>
+        </Router>
+    );
 };
 
 export default App;
