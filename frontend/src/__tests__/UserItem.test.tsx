4da1849d3b3e143988d855f5ccccee7cea8b2f99
mscbr <scibor.maciej@gmail.com>
2020-01-16 15:57:18 +0100

    test init

--- frontend/src/__tests__/UserItem.test.tsx
+++ frontend/src/__tests__/UserItem.test.tsx
@@ -2,3 +2,7 @@ import React from 'react';
 import { render, fireEvent, waitForElement } from '@testing-library/react';
 
 import UserItem, { Props } from '../user/components/UserItem';
+
+describe('<UserItem />', () => {
+    test('test test', async () => true);
+});
