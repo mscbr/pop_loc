326af65afc7519573a31449314dfd26f753070e9
mscbr <scibor.maciej@gmail.com>
2020-01-20 16:12:42 +0100

    styling fix

--- frontend/src/shared/components/ui-elements/Card.tsx
+++ frontend/src/shared/components/ui-elements/Card.tsx
@@ -5,19 +5,19 @@ import { secondary, accent2 } from '../../Styles';
 
 interface Props {
     horizontal?: boolean;
-    children: React.ReactNode | JSX.Element[] | JSX.Element;
+    children: JSX.Element[] | JSX.Element;
 }
 
 const StyledCard = styled.div`
     display: flex;
     flex-direction: ${(props: Props) => (props.horizontal ? 'row' : 'column')};
+    justify-content: space-around;
     background: ${secondary};
     border-radius: 5px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 `;
 
 const Card: React.FC<Props> = props => {
-    console.log('Card props', props);
-    return <StyledCard>{props.children}</StyledCard>;
+    return <StyledCard horizontal>{props.children}</StyledCard>;
 };
 export default Card;
