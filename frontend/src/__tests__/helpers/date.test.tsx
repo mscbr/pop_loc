a3fbf6c285caaa795436f6275c747c73f68051ee
mscbr <scibor.maciej@gmail.com>
2020-01-22 14:13:15 +0100

    batch rename

--- frontend/src/__tests__/helpers/date.test.tsx
+++ frontend/src/__tests__/helpers/date.test.tsx
@@ -1,7 +1,7 @@
 import * as date from '../../helpers/date';
 
 
-describe('display24time', () => {
+describe('date helpers tests', () => {
     test('24formatTest', () => {
         const testDate = new Date(2019, 8, 23, 10, 33, 30, 0);
         expect(date.display24time(testDate)).toBe('10:33');
