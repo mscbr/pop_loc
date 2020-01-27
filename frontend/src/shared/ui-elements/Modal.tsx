4b71e3025a111fe4a80aebef9fa7870a9c129ca3
mscbr <scibor.maciej@gmail.com>
2020-01-27 18:29:47 +0100

    styles init

--- frontend/src/shared/ui-elements/Modal.tsx
+++ frontend/src/shared/ui-elements/Modal.tsx
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import Backdrop from './Backdrop';
 
+import styled from 'styled-components';
+import { secondary } from '../Styles';
+
+const StyledModal = styled.div`
+    background: ${secondary};
+`;
+
 const Modal: React.FC = (props: any) => {
-    return <div></div>;
+    return (
+        <>
+            <StyledModal></StyledModal>
+        </>
+    );
 };
 
 export default Modal;
